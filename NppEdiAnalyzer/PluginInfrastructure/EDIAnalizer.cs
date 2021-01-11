86e185c8c9f417841a7046dd013836b076239318
innox80 <77303674+innox80@users.noreply.github.com>
2021-01-11 23:44:16 +0100

    Add files via upload

---
+++ NppEdiAnalyzer/PluginInfrastructure/EDIAnalizer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class ComponentStructure
+{
+    public string sTag { get; set; }
+    public string sDescription { get; set; }
+    public int iMinlength { get; set; }
+    public int iMaxLength { get; set; }
+    public string sDataType { get; set; }
+    public string sRefTable { get; set; }
+    public string sValue { get; set; }
+
+public ComponentStructure(string sTag, string sDescription, int iMinlength, int iMaxLength, string sDataType, string sRefTable, string sValue)
+    {
+        this.sTag = sTag;
+        this.sDescription = sDescription;
+        this.iMinlength = iMinlength;
+        this.iMaxLength = iMaxLength;
+        this.sDataType = sDataType;
+        this.sRefTable = sRefTable;
+        this.sValue = sValue;
+    }
+}
+
+public class ElementStructure
+{
+    public string sTag { get; set; }
+    public string sDescription { get; set; }
+    public string sValue { get; set; }
+    public string sRefTable { get; set; }
+
+    public List<ComponentStructure> ComponentList = new List<ComponentStructure>();
+
+    public ElementStructure(string sTag, string sDescription, string sValue, string sRefTable)
+    {
+        this.sTag = sTag;
+        this.sDescription = sDescription;
+        this.sValue = sValue;
+        this.sRefTable = sRefTable;
+    }
+}
+
+public class SegmentStructure
+{
+    public string sFileName { get; set; }
+    public int iLineNumber { get; set; }
+    public string sGroup { get; set; }
+    public int iGroupNumber { get; set; }
+    public string sTag { get; set; }
+    public string sDescription { get; set; }
+
+    public List<ElementStructure> ElementsList = new List<ElementStructure>();
+    //List<SegmentStructure> SegmentList = new List<SegmentStructure>();
+
+    public SegmentStructure(string sFileName, int iLineNumber, string sGroup, int iGroupNumber, string sTag, string sDescription)
+    {
+        this.sFileName = sFileName;
+        this.iLineNumber = iLineNumber;
+        this.sGroup = sGroup;
+        this.iGroupNumber = iGroupNumber;
+        this.sTag = sTag;
+        this.sDescription = sDescription;
+        //this.ElementsList = null;
+
+    }
+
+}
---
+++ NppEdiAnalyzer/PluginInfrastructure/Util.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Kbg.NppPluginNET;
+using Kbg.NppPluginNET.PluginInfrastructure;
+
+//namespace NppPluginNET.PluginInfrastructure
+//{
+    public static class Util
+    {
+        public static int FindNext(string  searchText, IScintillaGateway editor)
+        {
+            editor.SetSearchFlags(FindOption.NONE);
+            editor.SetTargetStart(Math.Max(editor.GetCurrentPos(), editor.GetAnchor()));
+            editor.SetTargetEnd(editor.GetTextLength());
+
+            int pos = editor.SearchInTarget(searchText.Length, searchText);
+            if (pos >= 0)
+            {
+                editor.SetSel(editor.GetTargetStart(), editor.GetTargetEnd());
+            }
+
+            return pos;
+        }
+
+        public static int ReplaceNext(string searchText, string replaceText, IScintillaGateway editor)
+        {
+            // Find the text and, if found, replace the selection
+            var pos = FindNext(searchText, editor);
+            if (pos >= 0)
+                editor.ReplaceSel(replaceText);
+            return pos;
+        }
+
+        public static void ReplaceAll(string searchText, string replaceText, IScintillaGateway editor)
+        {
+            var pos = 0;
+            // Iterate until text is no longer found
+            while (pos >= 0)
+            {
+                pos = ReplaceNext(searchText, replaceText, editor);
+            }
+        }
+
+
+
+}
+
+
+
+//}
