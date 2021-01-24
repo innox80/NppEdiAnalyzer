34930cc11d8c7593a06200adf26729f621064cbc
Innox <77303674+innox80@users.noreply.github.com>
2021-01-24 23:04:27 +0100

    Add files via upload

--- NppEdiAnalyzer/PluginInfrastructure/EDIAnalizer.cs
+++ NppEdiAnalyzer/PluginInfrastructure/EDIAnalizer.cs
@@ -67,4 +67,19 @@ public class SegmentStructure
 
     }
 
+
+
+
 }
+/*
+public class ComboboxItem
+{
+    public string Text { get; set; }
+    public object Value { get; set; }
+
+    public override string ToString()
+    {
+        return Text;
+    }
+}
+*/
